23e7611917175b038cc4355f17e510eaf4be3718
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-06 23:06:07 +0800

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    let out = "";
 
+    if (repeats < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < repeats; i++) {
+        out += string;
+    }
+
+    return out;
 };
 
 // Do not edit below this line
