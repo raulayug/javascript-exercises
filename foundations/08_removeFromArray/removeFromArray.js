ad9703210d1ed6abd9d8279c37fd42ca67359c6e
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-07 11:01:20 +0900

    Complete 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...to_remove) {
+    console.log(to_remove);
+
+    for (i in to_remove) {
+        while (arr.includes(to_remove[i])) {
+            let to_remove_index = arr.indexOf(to_remove[i]);
+            arr.splice(to_remove_index, 1);
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
