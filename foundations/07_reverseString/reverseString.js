aa63dc955c579fcb8dd1cc92dddb68caaff9da22
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-07 10:42:17 +0900

    Complete 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse_string = "";
 
+    for (let i = 1; i <= string.length; i++) {
+        reverse_string += string[string.length - i];
+    }
+
+    return reverse_string;
 };
 
 // Do not edit below this line
