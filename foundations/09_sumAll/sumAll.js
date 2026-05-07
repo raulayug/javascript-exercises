d66d570cac266cd2c61f686ac31837da7b0b5351
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-07 11:09:21 +0900

    Complete 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    if (!(Number.isInteger(a)) || !(Number.isInteger(b))) {
+        return "ERROR"
+    }
+    
+    let min = Math.min(a, b);
+    let max = Math.max(a, b);
+    let sum = 0;
 
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
