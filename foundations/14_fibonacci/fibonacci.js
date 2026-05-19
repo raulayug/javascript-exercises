9f8336055de679903f5f62087e0391afeefa29c5
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-19 13:40:53 +0800

    Complete 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) {
+        return "OOPS";
+    }
+    if (a < 1) {
+        return 0;
+    }
+    if (a < 3) {
+        return 1;
+    }
 
+    return (fibonacci(a-2) + fibonacci(a-1)); 
 };
 
 // Do not edit below this line
