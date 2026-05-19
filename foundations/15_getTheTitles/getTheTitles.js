d38a3312d378938e0cafb9c8405ad665f92e720e
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-19 13:55:29 +0800

    Complete 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let arr = [];
+    for (const index in books) {
+        arr.push(books[index].title);
+    }
+    return arr;
 };
 
 // Do not edit below this line
