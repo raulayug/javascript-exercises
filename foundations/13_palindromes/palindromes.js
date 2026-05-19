74c51ee35f4c9219bc1e3fe9066f17bf7c44ad10
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-19 13:24:51 +0800

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const valid = "abcdefghijklmnopqrstuvwxyz0123456789"
 
+    let arr = str.split('');
+
+    let mapped = arr.map(item => item.toLowerCase());
+    let filtered = mapped.filter(item => valid.includes(item));
+
+    let arr_str = filtered.join('');
+    let rev_str = filtered.slice().reverse().join('');
+
+    return arr_str === rev_str;
 };
 
 // Do not edit below this line
