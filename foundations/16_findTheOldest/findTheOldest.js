62c021d4a478adf7bdb9d8e57982c78a202fa1f4
Rau Layug <111621730+raulayug@users.noreply.github.com>
2026-05-19 14:17:52 +0800

    Complete 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.forEach(element => {
+        if (!("yearOfDeath" in element)) {
+            element.yearOfDeath = 2026;
+        }
+    });
+    let ans = arr.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0];
+    return ans;
 };
 
 // Do not edit below this line
